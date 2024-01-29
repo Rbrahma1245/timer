dcf6606ac30de429087f905609f550c59706c792
Rbrahma1245 <rbrahma1245@gmail.com>
2024-01-29 11:10:31 +0530

    added life cycle

---
+++ src/ClassComponent/LifeCycle.jsx
@@ -0,0 +1,33 @@
+import  { Component } from "react";
+
+class LifeCycle extends Component {
+  constructor() {
+    super();
+
+    this.state = { count: 0 };
+  }
+
+  componentDidMount() {
+    console.log("After Mounting");
+  }
+
+  componentDidUpdate() {
+    console.log("Updated");
+    this.state.count;
+  }
+  handleClick() {
+    console.log(this.state.count+1);
+    this.setState({ count: this.state.count + 1 });
+  }
+  render() {
+    return (
+      <div>
+        LifeCycle
+        <button onClick={this.handleClick.bind(this)}>Click</button>
+        {this.state.count}
+      </div>
+    );
+  }
+}
+
+export default LifeCycle;
